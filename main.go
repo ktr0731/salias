3b4d317f0c5a50d7c68dea314d09a9cf35b115ce
lycoris0731 <aizu.s1230022@gmail.com>
2017-04-16 22:58:42 +0900

    [MOD] Refactoring

--- main.go
+++ main.go
@@ -6,9 +6,10 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"strings"
+	"path/filepath"
 
 	"github.com/BurntSushi/toml"
+	homedir "github.com/mitchellh/go-homedir"
 )
 
 func main() {
@@ -21,38 +22,61 @@ func main() {
 	subCommand := os.Args[2]
 	args := os.Args[3:]
 
-	path := "./salias.toml"
-	if envPath := os.Getenv("SALIAS_PATH"); envPath != "" {
-		path = envPath
+	dir, err := homedir.Dir()
+	if err != nil {
+		log.Fatal(err)
 	}
-	bytes, _ := ioutil.ReadFile(path)
+	path := filepath.Join(dir, ".config", "salias", "salias.toml")
+	if envPath := os.Getenv("SALIAS_PATH"); envPath != "" {
+		if envPathAbs, err := filepath.Abs(envPath); err != nil {
+			log.Fatal(err)
+		} else if envPath != "" {
+			path = envPathAbs
+		}
+	}
 
-	var commands interface{}
-	err := toml.Unmarshal(bytes, &commands)
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		log.Fatalf("config path: %s not found", path)
+	} else if err != nil {
+		log.Fatal(err)
+	}
+
+	bytes, err := ioutil.ReadFile(path)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if commands, ok := commands.(map[string]interface{})[command]; ok {
-		if aliases, ok := commands.(map[string]interface{}); ok {
-			for k, alias := range aliases {
-				if k == subCommand {
-					newArgs := make([]string, 1+len(args))
-					newArgs[0] = alias.(string)
-					for i := range args {
-						newArgs[i+1] = args[i]
-					}
-					fmt.Println(command, strings.Join(newArgs, " "))
-					cmd := exec.Command(command, newArgs...)
-					cmd.Stdout = os.Stdout
-					cmd.Stderr = os.Stderr
-					err := cmd.Run()
-					if err != nil {
-						log.Fatal(err)
-					}
-				}
-			}
-		}
+	var commands interface{}
+	err = toml.Unmarshal(bytes, &commands)
+	if err != nil {
+		log.Fatal(err)
 	}
 
+	var ok bool
+	if commands, ok = commands.(map[string]interface{})[command]; !ok {
+		log.Fatal("type assertion failed")
+	}
+
+	var aliases map[string]interface{}
+	if aliases, ok = commands.(map[string]interface{}); !ok {
+		log.Fatal("type assertion failed2")
+	}
+
+	for k, alias := range aliases {
+		if k == subCommand {
+			newArgs := make([]string, 1+len(args))
+			newArgs[0] = alias.(string)
+			for i := range args {
+				newArgs[i+1] = args[i]
+			}
+			cmd := exec.Command(command, newArgs...)
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			if err = cmd.Run(); err != nil {
+				os.Exit(1)
+			}
+			os.Exit(0)
+		}
+	}
 }
